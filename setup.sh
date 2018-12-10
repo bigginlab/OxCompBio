d98776862b5b13c878bfc2306bc6e2d978b2ae05
IAlibay <irfan.alibay@manchester.ac.uk>
2018-12-10 10:49:27 +0000

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# Set up conda environment
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+bash Miniconda3-latest-MacOSX-x86_64.sh -b -f -p conda3
+
+export PATH=${PATH}:"${HOME}/conda3/bin"
+
+echo 'export PATH=${PATH}:"${HOME}/conda3/bin"' >> ~/.bashrc 
+
+# Create correct conda environment
+
 echo "source activate WTcourse" >> ~/.bashrc
 
 conda create -y --name WTcourse python=3.6
 source activate WTcourse
 
 conda install -y jupyter
-conda install -y mdanalysis numpy matplotlib
+conda install -y numpy
+conda install -y matplotlib
+conda install -y mdanalysis -c conda-forge
 conda install -y nglview -c bioconda
 jupyter-nbextension enable nglview --py --sys-prefix
 
