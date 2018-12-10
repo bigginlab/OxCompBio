029b3b38ef237612d873c6905fe8e7ea508b24ac
Irfan Alibay <IAlibay@users.noreply.github.com>
2018-12-10 11:34:21 +0000

    Changes the conda version to Linux

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Set up conda environment
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-bash Miniconda3-latest-MacOSX-x86_64.sh -b -f -p $HOME/conda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -f -p $HOME/conda3
 
 export PATH=${PATH}:"${HOME}/conda3/bin"
 
